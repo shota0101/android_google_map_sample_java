09c028479572a1430e0c2c14bb849759386e8e08
Shota Hayashi <qsezy1@hotmail.com>
2018-05-14 00:12:20 +0900

    初回起動時に大阪駅を表示

--- app/src/main/java/com/hayashi/android_google_map_sample_java/MapsActivity.java
+++ app/src/main/java/com/hayashi/android_google_map_sample_java/MapsActivity.java
@@ -3,6 +3,7 @@ package com.hayashi.android_google_map_sample_java;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -38,9 +39,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // Add a marker in Osaka Station and move the camera
+        LatLng latLng = new LatLng(34.702485, 135.495951);
+
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(latLng);
+        markerOptions.title("Osaka Station");
+        mMap.addMarker(markerOptions);
+
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(
+                latLng,
+                16);
+        mMap.moveCamera(cameraUpdate);
     }
 }
